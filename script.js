24b1bccd52430c461d2a4e29331b8fa02b6a4208
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2023-05-02 21:46:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const paragaf = document.getElementById("id");
 paragaf.style.color ="red";
-paragaf.style.position= "absolute";
-
+// paragaf.style.position= "absolute";
+paragaf.style.padding= "15px";
 
 
