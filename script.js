883c1fa0186ab036e23a3c15cbfbfec47085ab98
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2023-05-03 17:27:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+// Import stylesheets
+import './input.css';
 const paragaf = document.getElementById("id");
 paragaf.style.color ="red";
 // paragaf.style.position= "absolute";
 paragaf.style.padding= "15px";
+window.onscroll = function() {myFunction()};
 
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
 
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
